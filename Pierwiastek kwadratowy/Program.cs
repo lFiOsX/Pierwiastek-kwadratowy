0d8bf6a7ef4c13c7938f64bb4c23a88be3e6e2f8
unknown <pocztasebastian1306@gmail.com>
2023-03-26 08:14:14 +0200

    wyodrębnienie metody calcDelta, z metody calculateOfDeltaZeros

--- Pierwiastek kwadratowy/Program.cs
+++ Pierwiastek kwadratowy/Program.cs
@@ -1,13 +1,22 @@
 ﻿
+// 1,2,1 = 0
+// 1,1,-6 = 25
+// 1,0,1 =  -4
+
+
 using Spr_pierw_kwadrat;
 
 int a, b, c = 0;
 
 QuadraticEquation qe = new QuadraticEquation();
 
+Console.WriteLine("\t\tOBLICZANIE MIEJSC ZEROWYCH PIERWIASTKA KWADRATOWEGO\t\t\n\n");
+
+Console.WriteLine("Wzór: ax^2 + bx + c = 0\n");
 Console.WriteLine("Podaj \"a\":");
 a = int.Parse(Console.ReadLine());
 
+// Współczynnik a nie może równać się zero, gdy sie równa to powtórz.
 while (a == 0)
 {
     Console.WriteLine("Współczynnik \"a\" nie może być równy 0");
@@ -22,4 +31,8 @@ Console.WriteLine("Podaj \"c\":");
 c = int.Parse(Console.ReadLine());
 
 Console.WriteLine("\n");
-qe.calculationOfDeltaZeros(a, b, c);
+
+
+
+// obliczenie i zaprezentowanie wyników
+qe.calcDeltaZeros(a, b, c);
--- Pierwiastek kwadratowy/QuadraticEquation.cs
+++ Pierwiastek kwadratowy/QuadraticEquation.cs
@@ -8,26 +8,41 @@ namespace Spr_pierw_kwadrat
 {
     public class QuadraticEquation
     {
-        public void calculationOfDeltaZeros(int a, int b, int c)
+        public double calcDelta(int a, int b, int c)
         {
-            double delta, x0, x1, x2 = 0;
+            double delta = 0;
+            // obliczanie delty ze wzoru: D = b^2 - 4ac
             delta = b * b - 4 * a * c;
 
+            return delta;
+
+        }
+
+        public void calcDeltaZeros(int a, int b, int c)
+        {
+            double x0, x1, x2, delta = 0;
+
+            delta = calcDelta(a, b, c);
+
+            // Oblicznie miejsc zerowych dla poszczególnych przypadków.
             switch (delta)
             {
                 case < 0:
-                   
+                    //gdy delta < 0
                     Console.WriteLine($"delta = {delta}\n Delta mniejsza od zera, brak miejsc zerowych.");
                     break;
 
                 case 0:
-                  
+                    // gdy delta = 0
+                    // x0 = -b/2a
                     x0 = -b / 2 * a;
 
                     Console.WriteLine($"delta = {delta} \nMiejsce zerowe x0 = {Math.Round(x0, 2)}");
                     break;
                 case > 0:
-                    
+                    // gdy delta > 0
+                    // x1 = (-b-pierw(D))/2a
+                    // x2 = (-b+pierw(D))/2a
                     x1 = (-b - Math.Sqrt(delta)) / 2 * a;
                     x2 = (-b + Math.Sqrt(delta)) / 2 * a;
                     Console.WriteLine($"delta = {delta} \nMiejsca zerowe to:" +
@@ -35,6 +50,10 @@ namespace Spr_pierw_kwadrat
                     break;
 
             }
+
+
+
+
         }
     }
 }
