bb18ac4a468bbc161727138f0e81ab46317f0314
unknown <pocztasebastian1306@gmail.com>
2023-03-26 07:30:44 +0200

    dodoanie zmiennych i klasy QuadraticEquation

--- Pierwiastek kwadratowy/Program.cs
+++ Pierwiastek kwadratowy/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a, b, c = 0;
+
+a = int.Parse(Console.ReadLine());
+
+while (a == 0)
+{
+    Console.WriteLine("Współczynnik \"a\" nie może być równy 0");
+    Console.WriteLine("Podaj wartość \"a\" ponownie:");
+    a = int.Parse(Console.ReadLine());
+}
+
+b = int.Parse(Console.ReadLine());
+c = int.Parse(Console.ReadLine());
+
+Console.WriteLine("\n");
---
+++ Pierwiastek kwadratowy/QuadraticEquation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spr_pierw_kwadrat
+{
+    public class QuadraticEquation
+    {
+        
+        
+    }
+}
